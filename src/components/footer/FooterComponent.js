f25d44bf0848f04e9f31d583b598ee084fe6f151
prasadgade20 <prasadgade20@gmail.com>
2024-07-01 14:17:21 +0530

    Initial Commit

---
+++ src/components/footer/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <footer className='footer'>
+      <span>
+          &copy; Food Service Application @2024.
+      </span>
+    </footer>
+  )
+}
+
+export default FooterComponent
